a8f8b9b762b704471fbb7551b01a1689031d4097
aman deep <patnacityamandeep@gmail.com>
2022-01-05 11:33:37 +0530

    first commit

---
+++ function/amastrong.java
@@ -0,0 +1,25 @@
+package function;
+import java.util.Scanner;
+public class amastrong {
+
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number");
+        int a = sc.nextInt();
+        int c =a;
+        int sum =0;
+        while (a>0) {
+            int b = a%10;
+            b = b*b*b;
+            sum = sum+b;
+            a = a/10;
+        }
+        if(sum==c){
+            System.out.println("it is amastrong");
+        }
+        else{
+            System.out.println("it is not a amastrong");
+        }
+    }
+}
