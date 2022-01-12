1a7e956f4e6a1adbd609e65f32aaa9ca39b0fbf8
aman deep <patnacityamandeep@gmail.com>
2022-01-12 20:16:02 +0530

    leet ques

--- dsaNotes/validPerfectSqr.java
+++ dsaNotes/validPerfectSqr.java
@@ -18,5 +18,23 @@ public class validPerfectSqr {
         }
         return false;
     }*/
+    //Now applying binary search
+    static boolean perfectSqr(int num){
+        int start = 1, end =num;
+        while (start<=end){
+            int mid = start + (end-start)/2 ;
+            if(mid*mid == num){
+                return true;
+            }
+            else if(mid*mid< num){
+                start = mid+1;
+            }
+            else{
+                end = mid-1;
+            }
+        }
+        return false;
+
+    }
 
 }
