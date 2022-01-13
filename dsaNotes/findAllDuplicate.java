73c57f33e16ad757f36e3db2a0c838a7b851ba9d
aman deep <patnacityamandeep@gmail.com>
2022-01-14 02:18:51 +0530

    my first leet hard question

---
+++ dsaNotes/findAllDuplicate.java
@@ -0,0 +1,35 @@
+package dsaNotes;
+
+import java.util.ArrayList;
+
+public class findAllDuplicate {
+    public static void main(String[] args) {
+        int[] nums = {1,3,2};
+        ArrayList<Integer> ans = new ArrayList<>();
+        cyclicSort(nums);
+        for (int i =0;i< nums.length;i++){
+            if(nums[i] != i+1){
+                ans.add(nums[i]) ;
+            }
+        }
+        System.out.println(ans);
+    }
+    static void  cyclicSort(int[] arr){
+        int i =0;
+        while (i<arr.length){
+            int correct = arr[i]-1;
+            if(arr[i] != arr[correct]){
+                swap(arr,i,correct);
+            }
+            else {
+                i++;
+            }
+        }
+
+    }
+    static void swap(int[] arr,int a,int b){
+        int temp = arr[a];
+        arr[a] = arr[b];
+        arr[b] = temp;
+    }
+}
---
+++ dsaNotes/firstPositiveMissing.java
@@ -0,0 +1,38 @@
+package dsaNotes;
+
+import java.util.Arrays;
+
+public class firstPositiveMissing {
+    public static void main(String[] args) {
+        int[] nums = {3,4,-1,5};
+        cyclicSort(nums);
+        System.out.println(Arrays.toString(nums));
+        for(int i =0; i< nums.length;i++){
+            if(nums[i] != i+1){
+               // return i+1;
+                System.out.println(i+1);
+                break;
+            }
+        }
+     //   return nums.length +1;
+
+    }
+    static void cyclicSort(int arr[]){
+        int i =0;
+        while(i<arr.length){
+            int correct = arr[i]-1;
+            if (arr[i]>0 && arr[i]<=arr.length && arr[i]!=arr[correct]  ){
+                swap(arr,i,correct);
+            }
+            else{
+                i++;
+            }
+
+        }
+    }
+    static void swap(int[] arr, int a, int b){
+        int temp = arr[a];
+        arr[a] = arr[b];
+        arr[b] = temp;
+    }
+}
