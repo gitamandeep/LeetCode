d4af7d5ff8f78ad04f4d96fe1128e18b9213e16a
aman deep <patnacityamandeep@gmail.com>
2022-01-23 11:08:19 +0530

    binToDec programme

---
+++ dsaNotes/BinaryToDecimal.java
@@ -0,0 +1,14 @@
+package dsaNotes;
+
+public class BinaryToDecimal {
+    public static void main(String[] args) {
+        int[] num = {0,0,0,0};
+        int ans =0;
+        int base = 1;
+        for(int i = num.length-1;i>=0;i--){
+            ans += num[i]*base;
+            base = base*2;
+        }
+        System.out.println(ans);
+    }
+}
