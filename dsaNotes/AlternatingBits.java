d92af70e842def7ec0b368b3716ed84a2bc5eca1
aman deep <patnacityamandeep@gmail.com>
2022-01-26 00:44:47 +0530

    moreQues

---
+++ dsaNotes/AlternatingBits.java
@@ -0,0 +1,16 @@
+package dsaNotes;
+
+public class AlternatingBits {
+    public static void main(String[] args) {
+        int n =5;
+      while(n>1){
+          int b = n>>1;
+          if((n&1)== (b&1)){
+              System.out.println(false);
+          }
+          n = n>>1;
+      }
+
+        }
+    }
+
