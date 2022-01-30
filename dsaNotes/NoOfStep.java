8aa066d6994735ae1ffcc6238a2307d54c853e23
aman deep <patnacityamandeep@gmail.com>
2022-01-30 21:38:47 +0530

    recursion

---
+++ dsaNotes/NoOfStep.java
@@ -0,0 +1,19 @@
+package dsaNotes;
+
+public class NoOfStep {
+    public static void main(String[] args) {
+
+    }
+    public int func(int n){
+        return helper(n,0);
+    }
+    private int helper(int a, int count){
+        if(a==0){
+            return count;
+        }
+        if(a%2==0){
+            return helper(a-2,count+1);
+        }
+        return helper(a-1,count+1);
+    }
+}
